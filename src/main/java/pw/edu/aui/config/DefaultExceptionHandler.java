eb2773757dc8d2ecca0824329dad5131112be8f7
Delebrith <paulina.szwed.1996@gmail.com>
2020-12-07 00:56:05 +0100

    code cleaning: follow max redirects

--- src/main/java/pw/edu/aui/config/DefaultExceptionHandler.java
+++ src/main/java/pw/edu/aui/config/DefaultExceptionHandler.java
@@ -10,56 +10,18 @@ import pw.edu.aui.domain.ResourceNotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @ControllerAdvice
 @RequiredArgsConstructor
 public class DefaultExceptionHandler {
-    private static final String VISITED_LOCATIONS_HEADER = "x-locations-visited";
     private final RedirectConfig redirectConfig;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFound(final HttpServletRequest request, final ResourceNotFoundException e) {
         URI redirectUri = redirectConfig.getUriForResource(e.getId());
-//        return Optional.ofNullable(request.getHeader(VISITED_LOCATIONS_HEADER))
-//                .map(value -> value.split(";"))
-//                .map(array -> handleVisitedLocationsHeader(redirectUri, array))
-//                .orElse(this.handleNoVisitedLocationsHeader(redirectUri));
-
         return ResponseEntity
                 .status(HttpStatus.PERMANENT_REDIRECT)
                 .header(HttpHeaders.LOCATION, redirectUri.toString())
                 .build();
-
-//        return Optional.ofNullable(redirectConfig.getUriForResource(e.getId()))
-//                .map(uri -> ResponseEntity
-//                        .status(HttpStatus.PERMANENT_REDIRECT)
-//                        .header(HttpHeaders.LOCATION, uri.toString())
-//                        .build())
-//                .orElse(ResponseEntity.notFound().build());
-    }
-
-    private ResponseEntity handleNoVisitedLocationsHeader(URI redirectUri) {
-        return ResponseEntity
-                .status(HttpStatus.PERMANENT_REDIRECT)
-                .header(HttpHeaders.LOCATION, redirectUri.toString())
-                .header(VISITED_LOCATIONS_HEADER, redirectUri.toString())
-                .build();
-    }
-
-    private ResponseEntity handleVisitedLocationsHeader(URI redirectUri, String[] array) {
-        List<String> visitedURIs = Arrays.asList(array);
-        if (visitedURIs.contains(redirectUri.toString())) {
-            return ResponseEntity.notFound().build();
-        } else {
-            visitedURIs.add(redirectUri.toString());
-            return ResponseEntity
-                    .status(HttpStatus.PERMANENT_REDIRECT)
-                    .header(HttpHeaders.LOCATION, redirectUri.toString())
-                    .header(VISITED_LOCATIONS_HEADER, String.join(";", visitedURIs))
-                    .build();
-        }
     }
 }
